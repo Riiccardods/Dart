b06b18dcbd71f05ef90478e1fca11633ef443198
riicardods <riicardoljn@icloud.com>
2024-05-20 18:12:32 -0300

    first commit

---
+++ 01_hello_world.dart
@@ -0,0 +1,22 @@
+void main() {
+
+//     var myName = 'Ricardo';
+  
+//     late final myName = 'Ricardo';
+  
+       String myName = 'Ricardo';
+  
+  
+//      const myName = 'Ricardo';
+  
+//        myName = 'Ricardo2222222';
+  
+  
+      print('Hola ${myName}');
+  
+      print('Hola ${myName .toUpperCase()}');
+  
+      print('Hola ${ 1 + 1}');
+
+
+}
